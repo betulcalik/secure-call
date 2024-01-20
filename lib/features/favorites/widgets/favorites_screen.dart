9b508b4ab3cecc25b6becbdb967440993f4760d0
Betül Çalık <betulcalik82@icloud.com>
2024-01-20 16:21:31 +0300

    updated favorites screen

--- lib/features/favorites/widgets/favorites_screen.dart
+++ lib/features/favorites/widgets/favorites_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:secure_call/utils/custom_colors.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:secure_call/features/contacts/constants/contact_card_icon_type.dart';
+import '../../contacts/widgets/contact_card.dart';
+import '../bloc/favorites_bloc.dart';
+import '../bloc/favorites_event.dart';
+import '../bloc/favorites_state.dart';
 
 class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({super.key});
@@ -11,8 +16,22 @@ class FavoritesScreen extends StatefulWidget {
 class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("No favorite contacts found"),
+    return BlocBuilder<FavoritesBloc, FavoritesState>(
+      builder: (context, state) {
+        if (state is LoadedFavoriteContacts) {
+          if (state.contacts.isEmpty) {
+            return const Center(child: Text("No favorite contacts found"));
+          }
+          return ListView.builder(
+            itemCount: state.contacts.length,
+            itemBuilder: (context, index) =>
+              ContactCard(contact: state.contacts[index], iconType: ContactCardIconType.favorite),
+          );
+        } else {
+          // Handle the case where the state is not LoadedFavoriteContacts
+          return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
