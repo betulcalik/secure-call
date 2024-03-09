2ab3dae86d093e381edb0a4d25ef662f1f451b0a
Betül Çalık <betulcalik82@icloud.com>
2024-03-09 15:09:52 +0300

    updated registration repository apis

--- lib/features/registration/utils/registration_repository.dart
+++ lib/features/registration/utils/registration_repository.dart
@@ -37,25 +37,22 @@ class RegistrationRepository {
   }
 
   Future<Map<String, dynamic>> login(LoginModel model) async {
-    var response = await http.post(
-      Uri.parse('$url/login'),
-      body: {
-        'phone': model.phone,
-        'password': model.password,
-      },
-    );
+    Map<String, String> body = {
+      'phone': model.phone,
+      'password': model.password,
+    };
 
-    if (response.statusCode == 200) {
-      return {'success': true, 'message': 'Login successful'};
+    try {
+      http.Response response = await APIService.post("login", body);
+
+      if (response.statusCode ==  200 || response.statusCode ==  201) {
+        return {'success': true, 'message': 'Login successful'};
+      } else {
+        return {'success': false, 'message': 'Unexpected error occurred'};
+      }
+    } catch (e) {
+      return {'success': false, 'message': e};
     }
-
-    if (response.body.isNotEmpty) {
-      var errorResponse = jsonDecode(response.body);
-      String errorMessage = errorResponse['message'];
-      return {'success': false, 'message': errorMessage};
-    }
-
-    return {'success': false, 'message': 'Unexpected error occurred'};
   }
 
   Future<void> logout() async {
@@ -63,45 +60,39 @@ class RegistrationRepository {
   }
 
   Future<Map<String, dynamic>> verify(VerifyModel model) async {
-    var response = await http.post(
-      Uri.parse('$url/verify'),
-      body: {
-        'phone': model.phone,
-        'code': model.code,
-      },
-    );
+    Map<String, String> body = {
+      'phone': model.phone,
+      'code': model.code,
+    };
 
-    if (response.statusCode == 200) {
-      return {'success': true, 'message': 'Verify successful'};
+    try {
+      http.Response response = await APIService.post("verify", body);
+
+      if (response.statusCode ==  200 || response.statusCode ==  201) {
+        return {'success': true, 'message': 'Verify successful'};
+      } else {
+        return {'success': false, 'message': 'Unexpected error occurred'};
+      }
+    } catch (e) {
+      return {'success': false, 'message': e};
     }
-
-    if (response.body.isNotEmpty) {
-      var errorResponse = jsonDecode(response.body);
-      String errorMessage = errorResponse['message'];
-      return {'success': false, 'message': errorMessage};
-    }
-
-    return {'success': false, 'message': 'Unexpected error occurred'};
   }
 
   Future<Map<String, dynamic>> resendCode(ResendCodeModel model) async {
-    var response = await http.post(
-      Uri.parse('$url/resendCode'),
-      body: {
-        'phone': model.phone
-      },
-    );
+    Map<String, String> body = {
+      'phone': model.phone,
+    };
 
-    if (response.statusCode == 200) {
-      return {'success': true, 'message': 'Resend code successful'};
+    try {
+      http.Response response = await APIService.post("resendCode", body);
+
+      if (response.statusCode ==  200 || response.statusCode ==  201) {
+        return {'success': true, 'message': 'Resend code successful'};
+      } else {
+        return {'success': false, 'message': 'Unexpected error occurred'};
+      }
+    } catch (e) {
+      return {'success': false, 'message': e};
     }
-
-    if (response.body.isNotEmpty) {
-      var errorResponse = jsonDecode(response.body);
-      String errorMessage = errorResponse['message'];
-      return {'success': false, 'message': errorMessage};
-    }
-
-    return {'success': false, 'message': 'Unexpected error occurred'};
   }
 }
